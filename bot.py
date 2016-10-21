c563453f3b59a204effa36a73a5963bb92c514a5
Dmitry Gordeev <nillsondg@gmail.com>
2016-10-21 18:34:22 +0300

    implemented user storing in mongodb

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
+import datetime
 import hashlib
 import json
 from collections import OrderedDict
 
+import pymongo
 import telebot
 from telebot import types
 
 from api_token import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
-users = []
-auth_needed = False
+
+client = pymongo.MongoClient()
+db = client.users
+
 
 with open('state_map.json') as f:
     states = json.load(f, object_pairs_hook=OrderedDict)
@@ -37,18 +41,17 @@ def send_help(message):
 def send_welcome(message):
     markup = types.ReplyKeyboardHide(selective=False)
     bot.reply_to(message, "Привет!", reply_markup=markup)
-    username = message.from_user.username
-    if username not in users:
-        auth_request()
+    if not check_user_in_db(message.from_user):
         bot.send_message(message.chat.id, "Введите кодовое слово")
         bot.register_next_step_handler(message, check_auth)
+    else:
+        update_user_auth(message.from_user)
 
 
 def check_auth(message):
     if check_code(message.text):
-        users.append(message.from_user.username)
         bot.reply_to(message, "Добро пожаловать")
-        auth_granted()
+        add_user_into_db(message.from_user)
     else:
         bot.reply_to(message, "Неверно, попроуйте еще")
         bot.register_next_step_handler(message, check_auth)
@@ -58,17 +61,23 @@ def check_code(code):
     return hashlib.md5(code.encode('utf-8')).hexdigest() == "bc250e0d83c37b0953ada14e7bbc1dfd"
 
 
-def auth_request():
-    global auth_needed
-    auth_needed = True
+def check_user_in_db(user):
+    return db.users.find_one(str(user.id)) is not None
 
 
-def auth_granted():
-    global auth_needed
-    auth_needed = False
+def add_user_into_db(user):
+    db.users.insert({
+        "_id": str(user.id),
+        "last_name": user.last_name,
+        "first_name": user.first_name,
+        "username": user.username,
+        "added_date": datetime.datetime.utcnow(),
+        "last_auth": datetime.datetime.utcnow()
+    })
 
 
-user_dict = {}
+def update_user_auth(user):
+    db.users.update({'_id': str(user.id)}, {"$set": {'last_auth': datetime.datetime.utcnow()}})
 
 
 @bot.message_handler(commands=['cancel'])
@@ -118,5 +127,6 @@ def print_step_keyboard(msg, state):
         markup.add(types.KeyboardButton(button))
     bot.send_message(msg.chat.id, "Choose state:", reply_markup=markup)
     user_state[msg.chat.id] = STATE_SELECTOR
+
 
 bot.polling()
